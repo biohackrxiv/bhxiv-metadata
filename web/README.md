44d5d8a4d462e2ff981047acdfb8baf7d6171a04
Pjotr Prins <pjotr.public01@thebird.nl>
2020-11-14 13:58:53 +0000

    Creating small web service

---
+++ web/README.md
@@ -0,0 +1,9 @@
+# Small web app
+
+This sinatra application explores a few output screens. Run with
+
+    buntle exec app.rb
+
+And visit
+
+    http://localhost:4567/
---
+++ web/app.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+
+require 'net/http'
+require 'json'
+require 'ostruct'
+
+SPARQL_HEADER="
+prefix bhx: <http://biohackerxiv.org/resource>
+prefix dc: <http://purl.org/dc/elements/1.1/>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+"
+
+def sparql q, transform = nil
+  q = SPARQL_HEADER+q
+  p q
+  api_url = "http://sparql.genenetwork.org/sparql/?default-graph-uri=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+&query=#{ERB::Util.url_encode(q)}"
+
+  response = Net::HTTP.get_response(URI.parse(api_url))
+  data = JSON.parse(response.body)
+  vars = data['head']['vars']
+  results = data['results']['bindings']
+  results.map { |rec|
+    res = {}
+    vars.each { |name|
+      res[name.to_sym] = rec[name]['value']
+    }
+    if transform
+      transform.call(res)
+    else
+      res
+    end
+  }
+end
+
+get '/' do
+  'BioHackrXiv <a href="/list">examples</a>'
+end
+
+get '/list' do
+  bh = params[:bh]
+
+  QUERY = <<QUERY_BODY
+SELECT  ?url ?name
+FROM    <https://BioHackrXiv.org/graph>
+WHERE   {
+ ?url rdfs:label ?name
+}
+QUERY_BODY
+
+  # list = sparql(QUERY, lambda { |rec| rec } )
+  list = sparql(QUERY)
+  p list
+  biohackathons = {}
+  list.each { |rec| biohackathons[rec[:name]] = rec[:url] }
+  p biohackathons
+  papers = [ "TEST" ]
+  erb :list, :locals => { bh: bh, bhs: biohackathons, papers: papers }
+end
