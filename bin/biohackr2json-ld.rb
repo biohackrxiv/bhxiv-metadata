4877d95cae917721ace7eb17fc6604c6f8c12c3f
Pjotr Prins <pjotr.public01@thebird.nl>
2021-06-21 02:20:54 -0500

    Started on JSON-LD

---
+++ bin/biohackr2json-ld.rb
@@ -0,0 +1,61 @@
+#! /usr/bin/env ruby
+#
+# We use JSON-LD for exchanging metadata. For an explanation see, for
+# example: http://niem.github.io/json/tutorial/
+
+require 'yaml'
+require 'pp'
+require 'uri'
+require 'net/http'
+require 'erb'
+require 'fileutils'
+
+$stderr.print "BioHackrXiv metadata generator...\n"
+
+events = YAML.load_file('etc/events.yaml')['events']
+papers = YAML.load_file('etc/papers.yaml')['papers']
+
+HEADER = <<HEADER
+{
+  "@context": "https://biohackrxiv.org/resource/",
+  "id": "https://biohackrxiv.org/list",
+  "type": "publications",
+  "name": "Publication List",
+  "issuer": "https://biohackrxiv.org/",
+  "issued": "#{Time.new.to_s}",
+HEADER
+
+print HEADER
+
+print "\"events\": [\n"
+
+events.each_with_index do | e,i |
+  print "    ,\n" if i>0
+  name,event = e
+  print <<EVENT
+    {
+      "@id": "#{event['url']}",
+      "type": "event",
+      "name": "#{name}",
+      "description": "#{event['txt']}"
+    }
+EVENT
+end
+
+print "  ],\n"
+print "\"publications\": [\n"
+
+papers.each_with_index do |paper,i|
+  print "    ,\n" if i>0
+  print <<PAPER
+  {
+    "@id": "#{paper['id']}",
+    "type": "publication",
+    "event": "#{paper['event']}",
+    "doi": "#{paper['doi']}",
+    "url": "#{paper['url']}"
+  }
+PAPER
+end
+
+print "  ]}\n"
